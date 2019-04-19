5e94fbdf953b77d8a6356aa7503b89bedf940c9b
xecachan <mbin96@gmail.com>
2019-04-19 15:02:15 +0900

    ww

--- 7th-week/state-machine-#1.vhd
+++ 7th-week/state-machine-#1.vhd
@@ -1,7 +1,7 @@
 --스테이트머신을 잘 짜야 한다.
 --고퀄리티에 깔끔한 회로가 나온다.
-
---FSM finity state machine 유한 상태 머신
+--state코드 #1
+--state logic과 state register를 완전히 분리하여 코딩한다.
 
 library IEEE;
 use ieee.std_logic_1164.all;
@@ -19,11 +19,11 @@ entity stateMachine is
 end stateMachine;
 
 architecture BEHAVE of stateMachine is
-    signal nextState        :std_logic_vector (1 downto 0);
-    signal currentState     :std_logic_vector (1 downto 0);
+signal nextState        :std_logic_vector (1 downto 0);
+signal currentState     :std_logic_vector (1 downto 0);
 begin
-
-    process (X,Y,currentState)  --state logic
+    --state logic부분
+    process (X,Y,currentState)  --리셋 쓰면 안됨
     begin
         --케이스문으로 만든다.
         case currentState is
@@ -31,14 +31,16 @@ begin
             if X = '1' then
                 nextState <= "01";
             else
-                nextState <= "00";
+                nextState <= "00"; --변하지않는값(nextState <= currentState 해도 되긴함)
             end if;
         when "01" =>
             if X = '0' and Y = '1' then
                 nextState <= "00";
-            elsif Y = '0' then
+            elsif X = '0' and Y = '0' then
                 nextState <= "11";
             elsif X = '1' and Y = '1' then
+                nextState <= "10";
+            else
                 nextState <= "01";
             end if;
         when "10" =>
@@ -47,12 +49,16 @@ begin
             else 
             nextState < = "10";
             end if;
-        when others =>          --"11"일때
+        when others =>      --"11"일때
             if X = '0' and Y = '0' then 
                 nextState <= "00";
             else
                 nextState <= "11";
             end if;
+        --when "11" => 하고서
+        --when others =>
+        --        nextState <= "00"; 
+        --이렇게 짜는경우엔 조금더 안정적이된다
         end case;
     end process;
 
